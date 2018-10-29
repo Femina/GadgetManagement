cf4acf8072c968b26754901068b1080f65b083a3
Femina <femiwin10@gmail.com>
2018-10-29 07:32:51 +0400

    react

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 const requestIp = require('request-ip');
-
-
-
 
 const routes = {
 	products: {
 		get: '/api/products'
 	}
 };
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(cors());
 app.use(requestIp.mw())
@@ -19,11 +18,15 @@ app.get(routes.products.get, function (req, res) {
     res.sendFile(__dirname + '/client/data/products.json');
 });
 
-app.use('*', function (req, res) {
-    const ip = req.clientIp;
-    console.log(ip);
-    res.redirect(routes.products.get);
-});
+// app.use('*', function (req, res) {
+//     const ip = req.clientIp;
+//     console.log(ip);
+//     res.redirect(routes.products.get);
+// });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 
 app.listen(process.env.PORT || 5000)
